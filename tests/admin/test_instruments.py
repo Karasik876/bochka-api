d270a3be80edb3eb81cfc5529176f1bc9ead24ee
Yurchenko Stanislav <yurchenko.stas15@gmail.com>
2025-05-27 14:45:27 +0500

    tests: Fixed instruments creation tests

--- tests/admin/test_instruments.py
+++ tests/admin/test_instruments.py
@@ -10,19 +10,30 @@ from src.app.models import Instrument
 pytestmark = pytest.mark.asyncio(loop_scope="session")
 
 
-@pytest.mark.xfail
-async def test_create_instrument_success(
+async def test_delete_instrument_failed_401(
     db_session: AsyncSession,
     client: AsyncClient,
+):
+    ticker = "FAKE"
+
+    response = await client.delete(f"/admin/instrument/{ticker}")
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    assert response.json().get("error_code") == "authentication_failed"
+
+
+async def test_create_instrument_success(
+    db_session: AsyncSession,
+    admin_client: AsyncClient,
     admin_user: schemas.users.Read,
 ):
     instrument_data = {"ticker": "USD", "name": "Доллар США"}
-    response = await client.post("/admin/instrument", json=instrument_data)
+    response = await admin_client.post("/admin/instrument", json=instrument_data)
 
-    assert response.status_code == status.HTTP_201_CREATED
+    assert response.status_code == status.HTTP_200_OK
     json_response = response.json()
-    assert json_response.get("name") == "Доллар США"
-    assert json_response.get("ticker") == "USD"
+    assert json_response.get("success")
 
     result = await db_session.scalars(select(Instrument))
 
@@ -31,6 +42,7 @@ async def test_create_instrument_success(
     instr = res[0]
     assert isinstance(instr, Instrument)
     assert instr.ticker == "USD"
+    assert instr.name == "Доллар США"
 
 
 async def test_create_instrument_duplicate(
@@ -40,8 +52,7 @@ async def test_create_instrument_duplicate(
     instrument_data = {"ticker": "USD", "name": "Доллар США"}
     response = await admin_client.post("/admin/instrument", json=instrument_data)
 
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.json().get("ticker") == "USD"
+    assert response.status_code == status.HTTP_200_OK
 
     response = await admin_client.post("/admin/instrument", json=instrument_data)
 
@@ -61,20 +72,6 @@ async def test_delete_instrument_failed_404(
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
     assert response.json().get("error_code") == "resource_not_found"
-
-
-@pytest.mark.xfail
-async def test_delete_instrument_failed_401(
-    db_session: AsyncSession,
-    client: AsyncClient,
-):
-    ticker = "FAKE"
-
-    response = await client.delete(f"/admin/instrument/{ticker}")
-
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
-    assert response.json().get("error_code") == "authentication_failed"
 
 
 async def test_delete_instrument_failed_403(
