62ba7a31b4023ad84060dc4c991904bce7d04e7b
Nikita Kuzmin <guidemaster228@gmail.com>
2025-05-31 13:30:58 +0500

    fix: read_by_name exclude deleted

--- src/app/repositories/users.py
+++ src/app/repositories/users.py
@@ -8,10 +8,16 @@ class Users(core.repositories.sqlalchemy.BaseCRUD[models.User]):
     def __init__(self):
         super().__init__(models.User)
 
-    async def read_by_name(self, uow: core.UnitOfWork, name: str) -> models.User | None:
+    async def read_by_name(
+        self, uow: core.UnitOfWork, name: str, *, include_deleted: bool = False
+    ) -> models.User | None:
         try:
             session = uow.postgres_session
             query = select(self.model).where(self.model.name == name)
+
+            if not include_deleted and issubclass(self.model, core.models.sqlalchemy.SoftDelete):
+                query = query.where(self.model.deleted_at.is_(None))
+
             user = await session.scalar(query)
 
             if not user:
